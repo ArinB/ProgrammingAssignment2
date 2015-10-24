524c2d9b116107e9cd59f10f535231f3b3f07f46
ArinB <arin@Arindams-MacBook-Air-2.local>
2015-10-24 01:12:47 -0700

    Final Tested Working Codes of Assignment #2
    Final Tested Working Codes of Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m_inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    m_inverse <<- NULL
+  }
+  get <- function() x
+  getinverse <- function() solve(x)
+  setinverse <- function(current_inverse) m_inverse <<- current_inverse
+  list(set = set, get = get, getinverse = getinverse, setinverse = setinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m_cached_inverse <- x$getinverse()
+  if(!is.null(m_cached_inverse))
+    {
+      message("The cached inverse matrix is")
+      return(m_cached_inverse)
+    }
+      m_computed_inverse <- solve(x$get())
+      x$setinverse(m_computed_inverse)
+      m_computed_inverse
 }
